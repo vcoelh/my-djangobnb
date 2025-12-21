3fd70d236751c90094b81a5ef49a359715f2d454
Victor Coelho <victor.coelho@fgv.br>
2025-12-21 19:53:15 -0300

    finish AddPropertyModal frontend logic

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -1,36 +1,111 @@
 'use client';
 
 import Modal from "./Modal";
-
 import { useState } from "react";
-
 import CustomButton from "../forms/CustomButton";
-
 import useLoginModal from "../../hooks/useLoginModal";
-
+import { handleLogin } from "@/app/lib/actions";
+import apiService from "../services/apiService";
+import { useRouter } from "next/navigation";
 
 const LoginModal = () => {
-    const loginModal = useLoginModal()
+    const router = useRouter();
+    const loginModal = useLoginModal();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState<string[]>([]);
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const submitLogin = async () => {
+        setIsLoading(true);
+        setErrors([]);
+
+        const formData = {
+            email: email,
+            password: password
+        }
+
+        const response = await apiService.post('/api/auth/login/', JSON.stringify(formData));
+
+        if (response.access) {
+            handleLogin(response.user.pk, response.access, response.refresh);
+            loginModal.close();
+            router.push('/');
+        } else {
+            const tmpErrors: string[] = [];
+
+            if (response.non_field_errors) {
+                tmpErrors.push(...response.non_field_errors);
+            }
+
+            if (response.detail) {
+                tmpErrors.push(response.detail);
+            }
+
+            if (response.email) {
+                tmpErrors.push(...response.email);
+            }
+            if (response.password) {
+                tmpErrors.push(...response.password);
+            }
+
+            if (tmpErrors.length === 0) {
+                tmpErrors.push("Ocorreu um erro ao tentar fazer login.");
+            }
+
+            setErrors(tmpErrors);
+        }
+
+        setIsLoading(false);
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        submitLogin();
+    }
 
     const content = (
         <>
-            <form action="." className="space-y-4">
-                <input placeholder="Your email address" type="email"
-                    className="px-4 w-full h-[54px] border border-gray-300 rounded-xl" />
-                <input placeholder="Your password" type="password"
-                    className="px-4 w-full h-[54px] border border-gray-300 rounded-xl" />
+            <form
+                onSubmit={handleSubmit}
+                className="space-y-4"
+            >
+                <input
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Seu e-mail"
+                    type="email"
+                    disabled={isLoading}
+                    className="px-4 w-full h-[54px] border border-gray-300 rounded-xl disabled:opacity-50"
+                />
 
-                <div className="p-4 bg-airbnb text-white rounded-xl opacity-80">
-                    the error message
-                </div>
+                <input
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Sua senha"
+                    type="password"
+                    disabled={isLoading}
+                    className="px-4 w-full h-[54px] border border-gray-300 rounded-xl disabled:opacity-50"
+                />
+
+                {errors?.map((error, index) => {
+                    return (
+                        <div
+                            key={`error_${index}`}
+                            className="p-5 bg-airbnb text-white rounded-xl opacity-80"
+                        >
+                            {error}
+                        </div>
+                    )
+                })}
 
                 <CustomButton
-                    label="Submit"
+                    label={isLoading ? "Carregando..." : "Entrar"}
+                    onClick={submitLogin}
                 />
             </form>
         </>
     )
-
 
     return (
         <Modal
